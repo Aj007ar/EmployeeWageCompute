0bc4d72b7634b9afa7d4849af7e013e7d27cdd9d
Aj007ar <aj007.ar@gmail.com>
2022-08-10 10:59:06 +0530

    UC1 file added

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,7 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("*************WELCOME TO EMPLOYEE WAGE COMPUTATION*****************");
+            Console.WriteLine("1.UC1 Employee Attendance");
+            Console.WriteLine("\nEnter number for UC execute");
+            int check = Convert.ToInt32(Console.ReadLine());
 
+            switch (check)
+            {
+                case 1:
+                    Console.WriteLine("\n***************Employee Attendance***************************");
+                    UC1EmplyeeAttendance emplyeeAttendance = new UC1EmplyeeAttendance();
+                    emplyeeAttendance.CheckAttendance();
+                    break;
+            }
         }
     }
 }

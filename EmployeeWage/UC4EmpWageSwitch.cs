ed067d59e870492605a1f9626c3e89d97bcaa9cf
Aj007ar <aj007.ar@gmail.com>
2022-08-10 14:13:35 +0530

    UC4 files Added

--- EmployeeWage/UC4EmpWageSwitch.cs
+++ EmployeeWage/UC4EmpWageSwitch.cs
@@ -8,5 +8,34 @@ namespace EmployeeWage
 {
     internal class UC4EmpWageSwitch
     {
+        public void Empwage()
+        {
+            const int Full_Time = 1;
+            const int Part_Time = 2;
+            const int empRatePerHr = 20;
+            int empHr = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            switch (empCheck)
+            {
+                case Full_Time:
+                    empHr = 8;
+                    Console.WriteLine("employee is Present");
+                    break;
+                case Part_Time:
+                    empHr = 4;
+                    Console.WriteLine("employee is present for Half Day");
+                    break;
+                default:
+                    empHr = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
+
+            }
+            empWage = (empRatePerHr * empHr);
+            Console.WriteLine("Then Salary for employee is:" + empWage);
+        }
     }
 }

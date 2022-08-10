577be174970c991d4a7ca93ce02c08a02d757932
Aj007ar <aj007.ar@gmail.com>
2022-08-10 13:28:24 +0530

    UC2 file added

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("*************WELCOME TO EMPLOYEE WAGE COMPUTATION*****************");
             Console.WriteLine("1.UC1 Employee Attendance");
+            Console.WriteLine("2.UC2 Employee PartTime");
+
             Console.WriteLine("\nEnter number for UC execute");
             int check = Convert.ToInt32(Console.ReadLine());
 
@@ -15,6 +17,11 @@
                     Console.WriteLine("\n***************Employee Attendance***************************");
                     UC1EmplyeeAttendance emplyeeAttendance = new UC1EmplyeeAttendance();
                     emplyeeAttendance.CheckAttendance();
+                    break;
+                case 2:
+                    Console.WriteLine("\n*****************Part_Time/Full_Time Wage*********************");
+                    UC2PartTime partTime = new UC2PartTime();
+                    partTime.PartTimeWage();
                     break;
             }
         }
